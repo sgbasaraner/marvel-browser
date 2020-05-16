f2d9271a1b8ec24e5db6a94fa87db99f4f51bb5f
Sarp Basaraner <sarpbasaraner@gmail.com>
2020-05-16 07:14:33 +0300

    initialize collection view controller with flow layout

---
+++ marvel/CharacterCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CharacterCollectionViewCell.swift
+//  marvel
+//
+//  Created by Sarp on 16.05.2020.
+//  Copyright © 2020 Sarp. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    static let reuseId = "characterCell"
+    
+    func configure(name: String, imageUrl: String) {
+        nameLabel?.text = name
+        imageView?.image = UIImage(named: imageUrl)
+    }
+}
---
+++ marvel/CharactersViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CharactersViewController.swift
+//  marvel
+//
+//  Created by Sarp on 16.05.2020.
+//  Copyright © 2020 Sarp. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UICollectionViewController {
+    
+    var characters = dummyCharacters
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension CharactersViewController: UICollectionViewDelegateFlowLayout {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        characters.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView
+            .dequeueReusableCell(withReuseIdentifier: CharacterCollectionViewCell.reuseId,
+                                 for: indexPath) as? CharacterCollectionViewCell else { return UICollectionViewCell() }
+        let character = characters[indexPath.row]
+        cell.configure(name: character.name, imageUrl: character.imageUrl)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSize = (collectionView.frame.width - (collectionView.contentInset.left + collectionView.contentInset.right + 10)) / 2
+        return CGSize(width: itemSize, height: itemSize)
+    }
+}
